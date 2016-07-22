e99ffbcee91bf4f4814caee0cdd596cca8a320b3
Thomas Rogozinski <thomas.rogozinski@iselect.com.au>
2016-07-22 14:39:10 +1000

    Add project files.

---
+++ Logging/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+using log4net;
+using log4net.Config;
+
+namespace Logging {
+    class Program {
+        private static readonly log4net.ILog Logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        static void Main(string[] args) {
+            XmlConfigurator.Configure();
+            Logger.Info("Program started");
+
+            try {
+                //exception simulation
+                string test = null;
+                test = test.Trim();
+            } catch (Exception ex) {
+                Logger.Error("Unexpected Error", ex);
+            }
+
+            Logger.Info("Program finished");
+            LogManager.Shutdown();
+        }
+    }
+}
